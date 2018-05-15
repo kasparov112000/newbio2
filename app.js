62460c0a3930c39f1b8c8fcc8cc85b13bb48a0f7
Renato Nunez <212marines@gmail.com>
2018-05-14 20:35:03 -0700

    all files

--- app.js
+++ app.js
@@ -26,6 +26,21 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(session({ secret: 'Biolinkdepotorg', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true  }));
 
+
+// Access the session as req.session
+app.get('/', function(req, res, next) {
+  if (req.session.views) {
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + req.session.views + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session demo. refresh!')
+  }
+})
+
 if (!isProduction) {
   app.use(errorhandler());
 }
